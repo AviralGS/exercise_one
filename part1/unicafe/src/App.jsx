ebbc7acf96e6e7134894792e98fe6f0db9897cf7
AviralGS <aviral.sunar@gmail.com>
2024-10-22 14:02:30 +0100

    finished 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,43 @@
 import { useState } from 'react'
-const Positive = ({ text, value }) => <p>{text} {value}%</p>
-const Display = ({ text, value }) => <p>{text} {value}</p>
 
+const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+  return(
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td><StatisticLine text='Good' /></td>
+            <td><StatisticLine value={good} /></td>
+          </tr>
+          <tr>
+            <td><StatisticLine text='Neutral' /></td>
+            <td><StatisticLine value={neutral} /></td>
+          </tr>
+          <tr>
+            <td><StatisticLine text='Bad' /></td>
+            <td><StatisticLine value={bad} /></td>
+          </tr>
+          <tr>
+            <td><StatisticLine text='All' /></td>
+            <td><StatisticLine value={total} /></td>
+          </tr>
+          <tr>
+            <td><StatisticLine text='Average' /></td>
+            <td><StatisticLine value={average} /></td>
+          </tr>
+          <tr>
+            <td><StatisticLine text='Positive' /></td>
+            <td><StatisticLine value={positive} /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 const Button = ({ feedback, text }) => (
   <button onClick={feedback}>{text}</button>
@@ -13,7 +49,7 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const total = (good + neutral + bad)
   const average = ((good * 1) + (neutral * 0) + (bad * -1)) / total
-  const positive = good / total * 100
+  const positive = (good / total) * 100 + '%'
 
   return (
     <div>
@@ -23,12 +59,14 @@ const App = () => {
       <Button feedback={() => setBad(bad + 1)} text='Bad' />
       
       <h1>Statistics</h1>
-      <Display text='Good' value={good} />
-      <Display text='Neutral' value={neutral} />
-      <Display text='Bad' value={bad} />
-      <Display text='All' value={total} />
-      <Display text='Average' value={average} />
-      <Positive text='Positive' value={positive}/>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   )
 }
